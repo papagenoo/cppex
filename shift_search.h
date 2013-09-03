69748591f3a7933743a2b34b7a52deca8913f7a9
papagenoo <bmax@nm.ru>
2013-09-04 00:33:28 +0400

    !

--- shift_search.h
+++ shift_search.h
@@ -9,6 +9,7 @@ using namespace std;
 int binary_search(const vector<int>& vec, int p, int q, int f);
 // Поиск в массиве со сдвигом
 int shift_search(const vector<int>& vec, int p, int q, int f);
+// Запуск тектовой программы
 int shift_search_test();
 
 #endif // SHIFT_SEARCH_H
